e91787a1a5f67ebb0e9be9778b9dc3b3ddeb5326
igorthedci <42576193+igorthedci@users.noreply.github.com>
2018-09-14 22:05:09 +0300

    Update lesson03_task01_BankAccountTest.java

--- lesson03_task01_BankAccountTest.java
+++ lesson03_task01_BankAccountTest.java
@@ -4,37 +4,96 @@ import java.util.List;
 
 public class BankAccount_Test {
 
-    static long tmpAccountNumber; // = 987654321;
-    static double tmpBalance; // = 9999.99;
-    static BankAccount testAcc; // = new BankAccount(tmpAccountNumber, tmpBalance);
+    static long testAccountNumber; // = 987654321;
+    static double testBalance; // = 9999.99;
+    static BankAccount testAccount; // = new BankAccount(tmpAccountNumber, tmpBalance);
     private static final double DELTA = 1e-15;
 
     @BeforeClass
     public static void initClass() {
-        tmpAccountNumber = 987654321;
-        tmpBalance = 9999.99;
-        testAcc = new BankAccount(tmpAccountNumber, tmpBalance);
+        testAccountNumber = 987654321;
+        testBalance = 9999.99;
+        testAccount = new BankAccount(testAccountNumber, testBalance);
     }
 
     @Before
     public void init() {
-        testAcc.setAccountNumber(tmpAccountNumber);
-        testAcc.setBalance(tmpBalance);
+        testAccount.setAccountNumber(testAccountNumber);
+        testAccount.setBalance(testBalance);
     }
 
     @Test
     public void verifyCredit() {
         double posCredit = 999.99;
-        testAcc.credit(posCredit);
-        double actualResult = testAcc.getBalance();
-        double expectedResult = tmpBalance + posCredit;
+        testAccount.credit(posCredit);
+        double actualResult = testAccount.getBalance();
+        double expectedResult = testBalance + posCredit;
+
+        Assert.assertEquals(expectedResult, actualResult, DELTA);
+    }
+
+    @Test
+    public void verifyDebitPositiveCase01() {
+        double posDebit = 999.99;
+        testAccount.debit(posDebit);
+        double actualResult = testAccount.getBalance();
+        double expectedResult = testBalance - posDebit;
+
+        Assert.assertEquals(expectedResult, actualResult, DELTA);
+    }
+
+    @Test
+    public void verifyDebitPositiveCase02() {
+        double posDebit = testAccount.getBalance();
+        testAccount.debit(posDebit);
+        double actualResult = testAccount.getBalance();
+        double expectedResult = testBalance - posDebit;
+
+        Assert.assertEquals(expectedResult, actualResult, DELTA);
+    }
+    @Test
+    public void verifyDebitNegativeCase03() {
+        double expectedResult = testBalance;
+        double posDebit = testAccount.getBalance() + 1;
+        testAccount.debit(posDebit);
+        double actualResult = testAccount.getBalance();
+
+        Assert.assertEquals(expectedResult, actualResult, DELTA);
+    }
+    @Test
+    public void verifyDebitValidNegativeCase04() {
+        boolean expectedResult = false;
+        boolean actualResult = testAccount.debitValid(testBalance + 1);
+
+        Assert.assertEquals(expectedResult, actualResult);
+    }
+    @Test
+    public void verifyDebitValidPositiveCase05() {
+        boolean expectedResult = true;
+        boolean actualResult = testAccount.debitValid(testBalance);
+
+        Assert.assertEquals(expectedResult, actualResult);
+    }
+    @Test
+    public void verifyDepositPositiveCase06() {
+        double expectedResult = testBalance;
+        testAccount.deposit(0);
+        double actualResult = testAccount.getBalance();
+
+        Assert.assertEquals(expectedResult, actualResult, DELTA);
+    }
+    @Test
+    public void verifyDepositPositiveCase07() {
+        double expectedResult = 0.0;
+        testAccount.deposit(testBalance);
+        double actualResult = testAccount.getBalance();
 
         Assert.assertEquals(expectedResult, actualResult, DELTA);
     }
 
     @AfterClass
     public static void closeClass() {
-        testAcc = null;
+        testAccount = null;
     }
 
 }
