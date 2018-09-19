9a458db46f18528ffa9aacad0bf046110e593de6
igorthedci <42576193+igorthedci@users.noreply.github.com>
2018-09-19 22:30:26 +0300

    Create lesson04_task01.java

---
+++ lesson04_task01.java
@@ -0,0 +1,10 @@
+// 
+// TEST LINK = http://automationpractice.com/index.php
+// Create account page
+// 
+// В проекте создать класс MainPage. 
+// С помощью языка Xpath и CSS селекторов описать  локаторы для веб элементов
+//
+class MainPage {
+
+}
